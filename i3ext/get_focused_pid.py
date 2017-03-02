af2d92e178d84c9573ebc18ca4f0c44650cb8018
Emati Mitame <gitcommits@mita.me>
2017-03-02 22:20:26 +0000

    Remove/ignore some useless files

--- i3ext/get_focused_pid.py
+++
@@ -1,2 +0,0 @@
-def get_active_window_title(self):
-    root = Popen(["xprop", "-root", "_NET_ACTIVE_WINDOW"], stdout=PIPE
--- polybar/test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-i=1
-while true
-do
-  echo "%{F#5e81b1}%{F-} ${i}%"
-  i=$((i + 1))
-  sleep 1
-done
